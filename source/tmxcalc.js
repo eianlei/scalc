726e535f02f856884f4bdc6cc1d84482e7b9884a
Ian Leiman <ian.leiman@gmail.com>
2021-11-15 14:33:22 +0200

    tmxcalc refactor

--- source/tmxcalc.js
+++ source/tmxcalc.js
@@ -4,24 +4,53 @@
 // split the orginal function into two parts, this one doing just the math
 // the other one to generate text output from the results
 
+/**
+ * 
+ * @param {string} filltype ["air", "nx", "tmx", "pp", "cfm" ]
+ * @param {number} start_bar 
+ * @param {number} start_o2 
+ * @param {number} start_he 
+ * @param {number} stop_bar 
+ * @param {number} stop_o2 
+ * @param {number} stop_he 
+ * @param {boolean} he_ignore 
+ * @param {boolean} o2_ignore 
+ * @returns {tmxResult} 
+ */
 function tmxcalc_num(filltype, start_bar, start_o2, start_he, 
     stop_bar, stop_o2, stop_he, he_ignore, o2_ignore)
 {
     var tmxResult = {
         "status_code" : 99,
         "status_txt" : "FATAL ERROR\n",
-        add_air : 0,
-        add_o2 : 0,
-        add_he : 0,
-        add_nitrox : 0,
+        // copy the inputs because we need to pass them to tmxcalc_text();
+        filltype_in : filltype,
+        start_bar_in : start_bar, 
+        start_o2_in : start_o2, 
+        start_he_in : start_he, 
+        stop_bar_in : stop_bar, 
+        stop_o2_in : stop_o2, 
+        stop_he_in : stop_he,
+        //
+        add_air : 0, // bar of air to top in any method
+        add_o2 : 0,  // bar of Oxygen added in any method, not including O2 in air
+        add_he : 0,  // bar of Helium added in any method
+        add_nitrox : 0,  // bar of Nitrox added in "nx" method
+        nitrox_pct  : 0,
         add_tmx : 0,
-        tbar_2 : 0,
-        tbar_3 : 0,
-        tmx_preo2_pct : 0,
-        tmx_o2_pct : 0,
-        mix_o2_pct : 0,
-        mix_he_pct :0,
-        mix_n_pct : 0
+        tbar_2 : 0, // tbar_2 is the tank pressure after we have filled Helium with PP method
+        tbar_3 : 0, // tbar_3 is pressure after He+O2 before topping air
+        tmx_preo2_pct : 0, // % of Oxygen in chamber 1 in "tmx" CFM method
+        tmx_he_pct : 0,
+        tmx_o2_pct : 0, // % of Oxygen in chamber 2 in "tmx" CFM method
+        // 
+        mix_o2_pct : 0, // final mix O2 %
+        mix_he_pct : 0, // final mix He %
+        mix_n2_pct : 0, // final mix N2 %
+        // 
+        t2_o2_pct : 0, // tbar_2 mix O2 %
+        t2_he_pct : 0, // tbar_2 mix He %
+        t2_n2_pct : 0, // tbar_2 mix N2 %
 
     }
     // error checking for input values, anything wrong and we return an error & skip calculations
@@ -135,12 +164,13 @@ function tmxcalc_num(filltype, start_bar, start_o2, start_he,
         he_ignore = true;
     }
     ///////////// do the calculations
-    start_he_bar = start_bar * start_he / 100; // how many bars Helium in tank at start?
+    var start_o2_bar = start_bar * start_o2 / 100;     // how many bars O2 in tank at start?
+    var start_he_bar = start_bar * start_he / 100; // how many bars Helium in tank at start?
                                                 // two cases: for plain Nitrox and actual Trimix fill
     if (he_ignore)
     {
         // calculate for Nitrox fill, igonore end_he target, no Helium is going in
-        stop_he_bar = start_he_bar;             // not add any He, so at the end we have same He bar
+        var stop_he_bar = start_he_bar;             // not add any He, so at the end we have same He bar
         stop_he = 100 * start_he_bar / stop_bar; // % He after the tank is filled full
         add_he = 0;                            // no Helium will be added
     } // end he_ignore == True
@@ -153,7 +183,10 @@ function tmxcalc_num(filltype, start_bar, start_o2, start_he,
 
     // COMMON PART OF CALCULATIONS
     // tbar_2 is the tank pressure after we have filled Helium with PP method
-    tbar_2 = start_bar + add_he;
+    var tbar_2 = start_bar + add_he;
+    tmxResult.t2_o2_pct = start_o2_bar/tbar_2 *100; // tbar_2 mix O2 %
+    tmxResult.t2_he_pct = (start_he_bar + add_he)/tbar_2 *100; // tbar_2 mix He %
+    tmxResult.t2_n2_pct = 100 - (tmxResult.t2_o2_pct + tmxResult.t2_he_pct); // tbar_2 mix N2 %
     //if he_ignore : add_he=0, and then tbar_2 = start_bar
 
     // are we not adding any oxygen?
@@ -171,22 +204,26 @@ function tmxcalc_num(filltype, start_bar, start_o2, start_he,
         tbar_3 = stop_bar - add_air;                // tbar_3 is pressure after He+O2 is in before topping air
         add_o2 = tbar_3 - tbar_2;                  // how many bar O2 we need to fill before air
     }// end else
-    start_o2_bar = start_bar * start_o2 / 100;     // how many bars O2 in tank at start?
+
+
+    tmxResult.t3_o2_pct = (start_o2_bar + add_o2)/tbar_3 *100; // tbar_3 mix O2 %
+    tmxResult.t3_he_pct = (start_he_bar + add_he)/tbar_3 *100; // tbar_3 mix He %
+    tmxResult.t3_n2_pct = 100 - (tmxResult.t3_o2_pct + tmxResult.t3_he_pct); // tbar_3 mix N2 %
 
     // we have now calculated all output needed for pp fill case
     // now we can verify the end mix, we can later check if we get what we want
-    mix_o2_pct = (100 * (start_o2_bar + add_o2 + add_air * 0.21) / stop_bar);
-    mix_he_pct = 100 * (start_he_bar + add_he) / stop_bar;
-    mix_n_pct = 100 - mix_he_pct - mix_o2_pct;
+    var mix_o2_pct = (100 * (start_o2_bar + add_o2 + add_air * 0.21) / stop_bar);
+    var mix_he_pct = 100 * (start_he_bar + add_he) / stop_bar;
+    var mix_n2_pct = 100 - mix_he_pct - mix_o2_pct;
     // additional output needed for cfm fill case
-    add_nitrox = stop_bar - tbar_2;              // bars of Nitrox after Helium is in
-    stop_o2_bar = stop_bar * stop_o2 / 100;       // bars of Oxygen that the Nitrox needs to contain
-    nitrox_pct = 100 * ((stop_o2_bar - start_o2_bar) / add_nitrox); // %O2 of the Nitrox needed
+    var add_nitrox = stop_bar - tbar_2;              // bars of Nitrox after Helium is in
+    var stop_o2_bar = stop_bar * stop_o2 / 100;       // bars of Oxygen that the Nitrox needs to contain
+    var nitrox_pct = 100 * ((stop_o2_bar - start_o2_bar) / add_nitrox); // %O2 of the Nitrox needed
                                                                     // additional output needed for tmx fill case
-    add_tmx = stop_bar - start_bar;             // bars of TMX mix we need to fill
-    tmx_he_pct = 100 * (stop_he_bar - start_he_bar) / add_tmx;  // %He that the TMX needs to have
-    tmx_o2_pct = 100 * (stop_o2_bar - start_o2_bar) / add_tmx;  // %O2 that the TMX needs to have
-    tmx_preo2_pct = tmx_o2_pct * ((100 - tmx_he_pct) / 100);   // what the O2 analyzer shows at start
+    var add_tmx = stop_bar - start_bar;             // bars of TMX mix we need to fill
+    var tmx_he_pct = 100 * (stop_he_bar - start_he_bar) / add_tmx;  // %He that the TMX needs to have
+    var tmx_o2_pct = 100 * (stop_o2_bar - start_o2_bar) / add_tmx;  // %O2 that the TMX needs to have
+    var tmx_preo2_pct = tmx_o2_pct * ((100 - tmx_he_pct) / 100);   // what the O2 analyzer shows at start
 
 
     // error checking for results, anything wrong and we return error code
@@ -194,7 +231,7 @@ function tmxcalc_num(filltype, start_bar, start_o2, start_he,
     {
         tmxResult.status_code = 52;
         tmxResult.status_txt = `ERROR: Nitrox CFM O2% <21% cannot be made!\n` +
-                               ` - would require ${nitrox_pct}% O2 CFM input\n`;
+                               ` - would require ${nitrox_pct.toFixed(1)}% O2 CFM input\n`;
         return tmxResult;
     };
     if ((filltype == "cfm" | filltype == "nx") & nitrox_pct > 36)
@@ -202,7 +239,7 @@ function tmxcalc_num(filltype, start_bar, start_o2, start_he,
         tmxResult.status_code = 53;
         tmxResult.status_txt =
             "ERROR: Nitrox CFM O2% >36% cannot be made!\n" +
-            ` - would require ${nitrox_pct}% O2 CFM input\n`;
+            ` - would require ${nitrox_pct.toFixed(1)}% O2 CFM input\n`;
         return tmxResult;
     };
 
@@ -240,8 +277,8 @@ function tmxcalc_num(filltype, start_bar, start_o2, start_he,
         tmxResult.status_code = 62;
         tmxResult.status_txt =
         "ERROR: Blending this mix is not possible!\n" +
-        ` - starting O2% is ${start_o2}% and you want ${stop_o2}% \n` +
-        ` - removing ${add_o2} bar O2 is not possible \n`;
+        ` - starting O2% is ${start_o2.toFixed(1)}% and you want ${stop_o2.toFixed(1)}% \n` +
+        ` - removing ${add_o2.toFixed(1)} bar O2 is not possible \n`;
         return tmxResult;
     };
     if (add_air < 0)
@@ -250,7 +287,7 @@ function tmxcalc_num(filltype, start_bar, start_o2, start_he,
         tmxResult.status_txt =
         "ERROR: Blending this mix is not possible!\n" +
         " negative Air \n" +
-        "<add_he {add_he}, add_o2 {add_o2}, add_air {add_air}> \n";
+        `<add_he ${add_he}, add_o2 ${add_o2}, add_air ${add_air}> \n`;
         return tmxResult;
     };
     // all tests have passed so we return values of the calculation
@@ -261,124 +298,129 @@ function tmxcalc_num(filltype, start_bar, start_o2, start_he,
     tmxResult.add_o2 = add_o2;
     tmxResult.add_he = add_he;
     tmxResult.add_nitrox = add_nitrox;
+    tmxResult.nitrox_pct = nitrox_pct;
     tmxResult.add_tmx = add_tmx;
+    tmxResult.tmx_he_pct = tmx_he_pct;
+    tmxResult.tmx_o2_pct = tmx_o2_pct;
     tmxResult.tbar_2 = tbar_2;
     tmxResult.tbar_3 = tbar_3;
     tmxResult.tmx_preo2_pct = tmx_preo2_pct;
-    tmxResult.tmx_o2_pct = tmx_o2_pct;
     tmxResult.mix_o2_pct = mix_o2_pct;
     tmxResult.mix_he_pct = mix_he_pct;
-    tmxResult.mix_n_pct = mix_n_pct;
+    tmxResult.mix_n2_pct = mix_n2_pct;
     return tmxResult;
 }
 
-function tmxcalc_text(filltype, start_bar, start_o2, start_he,
-    mix_o2_pct, mix_he_pct, mix_n_pct,
-    add_o2, add_he, add_air,
-    tbar_3, stop_bar,
-    )
+function tmxcalc_text(result )
 {
     // initial strings to use later    
-    if (add_he > 0)
+    if (result.add_he > 0)
     {
-        he_fill = `From ${start_bar.toFixed(1)} bars add ${add_he.toFixed(1)} bar Helium,`;
+        var he_fill = `From ${result.start_bar_in.toFixed(1)} bars add `+
+        `${result.add_he.toFixed(1)} bar Helium to ${result.tbar_2.toFixed(1)} bar `+
+        `(${result.t2_o2_pct.toFixed(0)}/${result.t2_he_pct.toFixed(0)}/${result.t2_n2_pct.toFixed(0)}),` ;
     }
     else
     {
-        he_fill = " - no helium added";
+        var he_fill = " - no helium added";
     }
-    if (add_o2 > 0)
+    if (result.add_o2 > 0)
     {
-        o2_fill = `From ${tbar_2.toFixed(1)} bars add ${add_o2.toFixed(1)} bar Oxygen,`;
-    }
-    else
-    {
-        o2_fill = " - no oxygen added";
-    }
-    if (add_nitrox > 0)
-    {
-        nitrox_fill = `From ${tbar_2.toFixed(1)} bars add ${add_nitrox.toFixed(1)} bar ${nitrox_pct.toFixed(1)}% NITROX BY CFM,`;
+        var o2_fill = `From ${result.tbar_2.toFixed(1)} bars add `+
+        `${result.add_o2.toFixed(1)} bar Oxygen to ${result.tbar_3.toFixed(1)} bar `+
+        `(${result.t3_o2_pct.toFixed(0)}/${result.t3_he_pct.toFixed(0)}/${result.t3_n2_pct.toFixed(0)}) ,`;
     }
     else
     {
-        nitrox_fill = " - no Nitrox added";
+        var o2_fill = " - no oxygen added";
     }
-    if (add_tmx > 0)
+    if (result.add_nitrox > 0)
     {
-        tmx_fill = `From ${start_bar.toFixed(1)} bars add ${add_tmx.toFixed(1)} bar ${tmx_o2_pct.toFixed(1)}/${tmx_he_pct.toFixed(1)} TRIMIX BY CFM,`;   
+        var nitrox_fill = `From ${result.tbar_2.toFixed(1)} bars add ${result.add_nitrox.toFixed(1)} bar ${result.nitrox_pct.toFixed(1)}% NITROX BY CFM,`;
     }
     else
     {
-        tmx_fill = " - no Trimix added";
+        var nitrox_fill = " - no Nitrox added";
+    }
+    if (result.add_tmx > 0)
+    {
+        var tmx_fill = `From ${result.start_bar_in.toFixed(1)} bars add ${result.add_tmx.toFixed(1)} bar ${result.tmx_o2_pct.toFixed(1)}/${result.tmx_he_pct.toFixed(1)} TRIMIX BY CFM,`;   
+    }
+    else
+    {
+        var tmx_fill = " - no Trimix added";
     }
     // start mix string
-    if (start_bar > 0)
+    if (result.start_bar > 0)
     {
-        start_n2 = 100 - start_o2 - start_he;
-        start_mix = `Starting from ${start_bar} bar with mix ${start_o2.toFixed(1)}/${start_he}/${start_n2.toFixed(0)} (O2/He/N).`;
+        result.start_n2 = 100 - result.start_o2 - result.start_he;
+        var start_mix = `Starting from ${result.start_bar} bar with mix `+
+        `${result.start_o2.toFixed(1)}/${result.start_he}/${result.start_n2.toFixed(0)} (O2/He/N).`;
     }
     else
-        start_mix = "Starting from EMPTY TANK ";
+        var start_mix = "Starting from EMPTY TANK ";
 
     // result mix string
-    result_mix = `Resulting mix will be ${mix_o2_pct.toFixed(0)}/${mix_he_pct.toFixed(0)}/${mix_n_pct.toFixed(0)} (O2/He/N).`;
+    var result_mix = `Resulting mix will be ${result.mix_o2_pct.toFixed(0)}/${result.mix_he_pct.toFixed(0)}/${result.mix_n2_pct.toFixed(0)} (O2/He/N).`;
 
     // sanity check, what are we actually making and what not
-    if ((filltype == "tmx") && (add_he == 0))
+    if ((result.filltype_in == "tmx") && (result.add_he == 0))
     {
-        filltype = "nx"; // NOT TRIMIX, just plain NX by CFM
+        result.filltype_in = "nx"; // NOT TRIMIX, just plain NX by CFM
     }
-    if (add_o2 == 0 && add_he == 0)
+    if (result.add_o2 == 0 && result.add_he == 0)
     {
-        filltype = "air"; // we are filling only air for sure
+        result.filltype_in = "air"; // we are filling only air for sure
     }
 
     // finally build the output string 
-    if (filltype == "air")
+    var result_txt = "error";
+    if (result.filltype_in == "air")
     {
-        result = `${start_mix}\n` +
+        result_txt = `${start_mix}\n` +
         "PLAIN AIR FILL:\n" +
         " - no Helium \n" +
         " - no extra Oxygen \n" +
-        ` - From ${start_bar.toFixed(1)} bars add ${add_air.toFixed(1)} bar air to ${stop_bar.toFixed(1)} bar.  \n` +
+        ` - From ${result.start_bar_in.toFixed(1)} bars add ${result.add_air.toFixed(1)} bar air to`+
+        ` ${result.stop_bar_in.toFixed(1)} bar.  \n` +
         `${result_mix}\n`;
     }
-    else if (filltype == "nx")
+    else if (result.filltype_in == "nx")
     {
-        result = `${start_mix}\n` +
+        result_txt = `${start_mix}\n` +
         "Nitrox CFM FILL:\n" +
         " - no Helium \n" +
         " - Oxygen enriched \n" +
         `${nitrox_fill} \n` +
         `${result_mix}\n`;
     }
-    else if (filltype == "pp")
+    else if (result.filltype_in == "pp")
     {
-        result = `${start_mix}\n` +
+        result_txt = `${start_mix}\n` +
         "PARTIAL PRESSURE BLENDING:\n" +
         ` - ${he_fill}\n` +
         ` - ${o2_fill}\n` +
-        ` - From ${tbar_3.toFixed(1)} bars add ${add_air.toFixed(1)} bar air to ${stop_bar.toFixed(1)} bar.  \n` +
+        ` - From ${result.tbar_3.toFixed(1)} bars add ${result.add_air.toFixed(1)} bar air to ${result.stop_bar_in.toFixed(1)} bar.  \n` +
         `${result_mix}\n`;
     }
-    else if (filltype == "cfm")
+    else if (result.filltype_in == "cfm")
     {
-        result = `${start_mix}\n` +
+        result_txt = `${start_mix}\n` +
         "Pure Helium + Nitrox CFM blending:\n" +
         ` - ${he_fill}\n` +
         ` - ${nitrox_fill}\n` +
         `${result_mix}\n`;
     }
-    else if (filltype == "tmx")
+    else if (result.filltype_in == "tmx")
     {
-        result =
+        result_txt =
         `${start_mix}\n` +
         "TMX CFM blending:\n" +
         `${tmx_fill} \n` +
-        ` - first open helium flow and adjust O2 to ${tmx_preo2_pct.toFixed(1)}% \n` +
-        ` - then open oxygen flow and adjust O2 to ${tmx_o2_pct.toFixed(1)}% \n` +
+        ` - first open helium flow and adjust O2 to ${result.tmx_preo2_pct.toFixed(1)}% \n` +
+        ` - then open oxygen flow and adjust O2 to ${result.tmx_o2_pct.toFixed(1)}% \n` +
         `${result_mix} \n`;
     }
 
-    return result;
+    return result_txt;
 }
